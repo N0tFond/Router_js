c7a145e312990deaa075ec37ff473ae2210ac23d
Not <simond.hugo@gmail.com>
2024-11-01 13:01:58 +0100

    Fix -> Route and add a comment

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AboutView from '@/views/AboutView.vue'
+import LoaderView from '@/views/LoaderView.vue'
+import FormView from '@/views/FormView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,18 +15,22 @@ const router = createRouter({
     {
       path: '/about',
       name: 'about',
-      component: () => import('../views/AboutView.vue')
+      //? On à pas besoin de la ligne 'component: () => import('../views/AboutView.vue')' on peut tout simplement la remplacer par le "component" suivi du nom du component à mettre en place
+      // component: () => import('../views/AboutView.vue')
+      component: AboutView
 
     },
     {
       path: '/loader',
       name: 'loader',
-      component: () => import('../views/LoaderView.vue')
+      // component: () => import('../views/LoaderView.vue')
+      component: LoaderView
     },
     {
       path: '/forms',
       name: 'forms',
-      component: () => import('../views/FormView.vue')
+      // component: () => import('../views/FormView.vue')
+      component: FormView
     }
   ]
 })
